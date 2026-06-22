bbe33dfaaa26fe87d48452c9416d6bbb60a311f1
william <wdpgrandos@gmail.com>
2026-06-21 19:27:52 -0500

    funcion suma terminada

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,10 +1,15 @@
 #include <oistream>
 
 using namespace std;
+int suma(int a, int b)
+{
+    return a+b;
+}
 
 int main()
 {
     count <<"hola clase">>
+    cout <<"la suma de 1 y 2 es" <<suma(1,2) << "!!";
     return 0;
     
 }
