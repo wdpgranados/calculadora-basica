678516da48a29c6e16879bfd48928e045aa3cfc2
william <wdpgrandos@gmail.com>
2026-06-21 20:40:40 -0500

    la funcion resta terminada

--- calculadora.cpp
+++ calculadora.cpp
@@ -6,10 +6,16 @@ int suma(int a, int b)
     return a+b;
 }
 
+int resta(int a, int b)
+{
+    return a-b;
+}
+
 int main()
 {
     count <<"hola clase">>
     cout <<"la suma de 1 y 2 es" <<suma(1,2) << "!!";
+    cout <<"la resta de 2 y 1 es" <<resta(2,1) << "!!";
     return 0;
     
 }
