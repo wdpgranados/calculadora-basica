b46cc61402eba364065f577b9446a19ff5861058
william <wdpgrandos@gmail.com>
2026-06-21 20:49:29 -0500

    mejorarando el texto

--- calculadora.cpp
+++ calculadora.cpp
@@ -14,8 +14,8 @@ int resta(int a, int b)
 int main()
 {
     count <<"hola clase">>
-    cout <<"la suma de 1 y 2 es" <<suma(1,2) << "!!";
-    cout <<"la resta de 2 y 1 es" <<resta(2,1) << "!!";
+    cout <<"la suma de 1 y 2 es" <<suma(1,2) << "!!"<< end1;
+    cout <<"la resta de 2 y 1 es" <<resta(2,1) << "!!"<< end1;
     return 0;
     
 }
